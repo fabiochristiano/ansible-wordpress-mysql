ae94a99785a821e267cabcaa7bb0c42f125a04ee
Fabio Christiano <fabio.augusto.christiano@gmail.com>
2018-06-05 23:10:32 -0300

    Add host in known_hosts

--- configure_inventory.sh
+++ configure_inventory.sh
@@ -1,5 +1,14 @@
 path=$(pwd)
-rm -rf hosts
+
+if [ -f "./hosts" ]; then
+  rm hosts
+fi  
+
+ssh-keygen -R "172.17.177.40"
+ssh-keygen -R "172.17.177.42"
+ssh-keyscan -H 172.17.177.40 >> ~/.ssh/known_hosts
+ssh-keyscan -H 172.17.177.42 >> ~/.ssh/known_hosts
+
 
 echo "[wordpress]" > hosts
 echo "172.17.177.40 ansible_user=vagrant ansible_ssh_private_key_file=\"${path}/.vagrant/machines/wordpress/virtualbox/private_key\"" >> hosts
